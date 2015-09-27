8eee2733f88bd5d73bdeaafe6d43850637ca319c
Devika <devikakishnan@gmail.com>
2015-09-27 16:06:24 -0400

    Ask input from user for number of nodes. Tried using 'for' loops.

---
+++ Tree1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 15:00:42 2015
+
+@author: Devi
+"""
+
+#Tree
+import string
+import numpy as np
+string.whitespace
+a = raw_input("Please Provide nodes of tree")
+node = int(a)
+root = ()
+tree = ()
+child = ()
+for i in range(1,node+1):
+   if i == 1:
+       d = 1
+       print d
+   elif i==2:
+       e = [d, d]
+       print e
+
+    
