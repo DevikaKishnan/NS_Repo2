c9c01291e11f64e236d5518a327e33f9dac4d42b
Devika <devikakishnan@gmail.com>
2015-09-28 21:43:22 -0400

    Create tree using functions

--- Tree1.py
+++ Tree1.py
@@ -6,24 +6,46 @@ Created on Fri Sep 25 15:00:42 2015
 """
 
 #Tree
-import string
-import numpy as np
-import operator as op
-#a = raw_input("Please Provide nodes of tree")
-node = 5
-#root = ()
-#parent = ()
-#child = ()
-b = []
-c = []
-b.append([0])
-for i in range(1,node+1):
-    b.append([i])
-    s1 = op.add(b[i-1],b[i])
-    c.append(s1)
-print b
-print c
-    
+# Creating a tree
+
+class exprnode(object):
+    def __init__(self, value = None, left = None, right  = None):
+        self.value = value
+        self.left = left
+        self.right = right
+        
+    def evaluate(self):
+        if self.value == '+':
+            ret1 = self.left.evaluate() + self.right.evaluate()
+            print 
+            return ret1 
+        elif self.value == '-':
+            ret2 = self.left.evaluate() - self.right.evaluate()
+            print ret2
+            return ret2
+        elif self.value == '*':
+            ret3 = self.left.evaluate()*self.right.evaluate()
+            print ret3
+            return ret3
+        elif self.value == '/':
+            ret4 = self.left.evaluate()/self.right.evaluate()
+            print ret4
+            return ret4
+        else:
+            print self.value
+            return self.value
+
+s = exprnode(1)
+t = s.evaluate()
+
+  
+
+
+            
+#my_expr = exprnode('*', exprnode('+',exprnode(2),exprnode(3)),exprnode(4))
+
+
+
 #    child[1] = [1]
 #    child[2] = [1,1]
 #    child[3] = [child[2][0],child[2][0] + child[2][1], child[2][1] + child[2][0], child[2][1]]
