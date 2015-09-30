f787fe295df6c152d7f2958d939abe956f9fa61d
Devika <devikakishnan@gmail.com>
2015-09-29 20:32:53 -0400

    Used class objects and for loops to take in a value and return addition of left/right siblings and value

--- Tree1.py
+++ Tree1.py
@@ -8,40 +8,44 @@ Created on Fri Sep 25 15:00:42 2015
 #Tree
 # Creating a tree
 
-class exprnode(object):
-    def __init__(self, value = None, left = None, right  = None):
+nodes = int(raw_input('Give nodes of tree'))
+value = int(raw_input('Give value'))
+
+class makelevel(object):
+    def __init__(self, value = None, nodes = None):
         self.value = value
-        self.left = left
-        self.right = right
+        self.nodes = nodes
+        self.left = 0
+        self.right = 0
+        
+    def leftsib(self):
+        self.left = self.value + self.left
+        return self.left
+        
+    def rightsib(self):
+        self.right = self.value + self.right 
+        return self.right
         
     def evaluate(self):
-        if self.value == '+':
-            ret1 = self.left.evaluate() + self.right.evaluate()
-            print 
-            return ret1 
-        elif self.value == '-':
-            ret2 = self.left.evaluate() - self.right.evaluate()
-            print ret2
-            return ret2
-        elif self.value == '*':
-            ret3 = self.left.evaluate()*self.right.evaluate()
-            print ret3
-            return ret3
-        elif self.value == '/':
-            ret4 = self.left.evaluate()/self.right.evaluate()
-            print ret4
-            return ret4
-        else:
-            print self.value
-            return self.value
-
-s = exprnode(1)
-t = s.evaluate()
-
-  
-
-
+        self.left == self.leftsib() 
+        self.right == self.rightsib() 
+        a  = [self.leftsib(), self.rightsib()]
+        return a
+    
+                
+for i in range(nodes):
+    c = makelevel.evaluate(makelevel(value,i))
+    print c
+    for j in range(2):
+        value = c[j]
+        e = makelevel(value,j)
+        f = makelevel.evaluate(e)
+        print f[0],' ',f[1]
             
+
+
+
+
 #my_expr = exprnode('*', exprnode('+',exprnode(2),exprnode(3)),exprnode(4))
 
 
